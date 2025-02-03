f0c92a997f9f18ba7d74f53a84d9a97b466f49ef
Stikl <alfrednewhot115@gmail.com>
2025-02-03 11:02:05 -0500

    actualizacion

---
+++ Javascript/proyectos.js
@@ -0,0 +1,34 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const botonesContrato = document.querySelectorAll('button.c1, button.c2, button.c3');
+
+    const divsContrato = document.querySelectorAll('div.c1, div.c2, div.c3');
+
+    const botonesNone = document.querySelectorAll('button#none');
+  
+
+    botonesContrato.forEach(boton => {
+      boton.addEventListener('click', () => {
+
+        const claseBoton = boton.classList[0];
+
+        divsContrato.forEach(div => div.hidden = true);
+
+        document.querySelector(`div.${claseBoton}`).hidden = false;
+      });
+    });
+  
+
+    botonesNone.forEach(boton => {
+      boton.addEventListener('click', () => {
+
+        const divPadre = boton.parentNode;
+
+        divPadre.hidden = true;
+      });
+    });
+  });
+
